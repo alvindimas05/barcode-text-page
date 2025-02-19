471a77634dca18b828a0fa544a80774b72a3d239
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-02-19 18:54:16 +0700

    fix: manual input and screenshot size

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2488638200"
+    serviceWorkerVersion: "1028101012"
   }
 });
