17130a85576ced1c4ec3e1ab4692d615133ab07d
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-02-24 20:41:19 +0700

    feat: imei input

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1012967935"
+    serviceWorkerVersion: "144914300"
   }
 });
